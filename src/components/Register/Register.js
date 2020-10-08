e52000ed79eca03dc0ed2ff5523b4aa3a3fadfab
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-09 01:22:28 +0600

    stuck in date

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,33 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../../App';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 const Register = () => {
+    const classes = useStyles();
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const [vs, setVs] = useState([]);
+    // const [details, setDetails] = useState({
+    //     description: document.getElementById('description').value,
+    // })
+    const [selectedDate, setSelectedDate] = useState({
+        regDate : new Date(),
+    })
+    const handleDate = (date) => {
+        const newDate = {...selectedDate}
+        newDate.regDate = date;
+        setSelectedDate(newDate);
+    } 
     const {id} = useParams();
     useEffect(() =>{
        fetch(`http://localhost:3001/vservice/${id}`)
@@ -14,22 +37,37 @@ const Register = () => {
         setVs(data);
        })
    },[])
-
+   
+   const handleRegistration = () => {
+       const addUser = {...loggedInUser, ...vs, ...selectedDate};
+       fetch('http://localhost:3001/addUser', {
+           method: 'POST',
+           headers:{'content-type': 'application/json'},
+           body: JSON.stringify(addUser)
+       })
+       .then(res => res.json())
+       .then(data =>{
+           console.log(data)
+       })
+   }
     return (
         <div>
             <h1>Register as a Volunteer</h1>
-            <form action="http://localhost:3001/addUser" method="post">
-                <input type="text" placeholder={loggedInUser.name} name="name"/>
+            <form action="" method="POST" className={classes.root} noValidate autoComplete="off">
+                <TextField id="standard-basic" label={loggedInUser.name} />
+                <br/>
+                <TextField id="standard-basic" label={loggedInUser.email} />
                 <br/>
                 <input type="text" placeholder={loggedInUser.email} name="email"/>
                 <br/>
-                <input type="text" name="date"/>
+                <label for="date">Date:</label>
+                <input type="date" format="dd-MM-yyyy" name="date" value={selectedDate.regDate} onChange={handleDate}/>
                 <br/>
-                <input type="text" placeholder="Description" name="description"/>
+                <input type="text" placeholder="Description" id="description" name="description" />
                 <br/>
                 <input type="text" placeholder={vs.title} name="title"/>
                 <br/>
-                <button type="submit">Register</button>
+                <button onClick={handleRegistration}>Register</button>
             </form>
         </div>
     );
